b6e11f00d43fbc772ddaff19a5d6a04c20bf0cf9
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-05-06 23:36:20 -0400

    Added functions for the basic math operations. Add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(num1, num2) {
+    let sum = num1 + num2;
+    return sum
+}
+
+function subtract(num1, num2) {
+    let difference = num1 - num2;
+    return difference;
+}
+
+function multiply(num1, num2) {
+    let product = num1 * num2
+    return product;
+}
+
+function divide(num1, num2) {
+    let quotient = num1 / num2
+    return quotient;
+}
+
+
+console.log(add(10,2))
+console.log(subtract(10,2))
+console.log(multiply(10,2))
+console.log(divide(10,2))
