47adefda08714afc5e1ccd71b33fda96dcea6108
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-05-06 23:46:45 -0400

    Added an operate function that returns the function depending on the operation that was submitted

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 function add(num1, num2) {
-    let sum = num1 + num2;
-    return sum
+  let sum = num1 + num2;
+  return sum;
 }
 
 function subtract(num1, num2) {
-    let difference = num1 - num2;
-    return difference;
+  let difference = num1 - num2;
+  return difference;
 }
 
 function multiply(num1, num2) {
-    let product = num1 * num2
-    return product;
+  let product = num1 * num2;
+  return product;
 }
 
 function divide(num1, num2) {
-    let quotient = num1 / num2
-    return quotient;
+  let quotient = num1 / num2;
+  return quotient;
 }
 
+// console.log(add(10, 2));
+// console.log(subtract(10, 2));
+// console.log(multiply(10, 2));
+// console.log(divide(10, 2));
 
-console.log(add(10,2))
-console.log(subtract(10,2))
-console.log(multiply(10,2))
-console.log(divide(10,2))
+function operate(number1, operator, number2) {
+  if (operator === "+") {
+    return add(number1, number2);
+  } else if (operator === "-") {
+    return subtract(number1, number2);
+  } else if (operator === "*") {
+    return multiply(number1, number2);
+  } else if (operator === "/") {
+    return divide(number1, number2);
+  }
+}
+console.log(operate(8, "/", 4))
