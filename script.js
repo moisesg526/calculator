edd3ceedb7d126e9102a11d5a4d6fa8103a93cd6
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-05-11 23:32:25 -0400

    Changed displayNumber function to show multiple numbers if they are being clicked before the operation

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
 let btn = document.querySelectorAll(".numBtn");
 let operatorBtn = document.querySelectorAll(".operatorBtn");
 let display = document.querySelector(".display");
-let firstClick = true;
+let answer = document.querySelector(".equals");
 
-let displayValue1;
-let displayValue2;
+// let firstClick = true;
+
+let displayValue = [];
 let operationValue;
 
 Array.from(btn).forEach((button) => {
-  button.addEventListener("click", displayNumber1);
+  button.addEventListener("click", displayNumber);
 });
 
 Array.from(operatorBtn).forEach((button) => {
   button.addEventListener("click", operation);
 });
 
-function mathProblem(e) {
-  let value = e.target.value;
-  if (value === "+") {
-    console.log(`value : ${Number(displayValue1) + Number(displayValue1)}`);
-  }
-}
+answer.addEventListener("click", operate);
 
-function displayNumber1(e) {
-  let value = e.target.value;
-  if (displayValue1 === undefined) {
-    displayValue1 = value;
-  } else {
-    value = displayValue1 += displayValue1;
-  }
-  display.textContent = value;
-  console.log(displayValue1);
-}
+// function mathProblem(e) {
+//   let value = e.target.value;
+//   if (value === "+") {
+//     console.log(`value : ${Number(displayValue) + Number(displayValue)}`);
+//   }
+// }
 
-function displayNumber2(e) {
+function displayNumber(e) {
   let value = e.target.value;
-  displayValue1 = value;
-  display.textContent = value;
-  console.log(displayValue1);
+  if (
+    e.target.value !== "÷" ||
+    e.target.value !== "X" ||
+    e.target.value !== "-" ||
+    e.target.value !== "+"
+  ) {
+    displayValue.push(value);
+    display.textContent = displayValue;
+    console.log(displayValue);
+  } else if (e.target.value1 == "operation") {
+    operationValue = e.target.value;
+    console.log(operationValue);
+  }
 }
 
 function operation(e) {
@@ -76,5 +78,5 @@ function operate(number1, operator, number2) {
   } else if (operator === "/") {
     return divide(number1, number2);
   }
+  console.log("operate is running");
 }
-// console.log(operate(160, "/", 4));
