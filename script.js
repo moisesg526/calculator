db1ba5437d7ea49e8f70ee37c014e5f6c2d99373
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-05-21 22:45:03 -0400

    Changed the function to run the display values through the Number constructor

--- script.js
+++ script.js
@@ -43,34 +43,44 @@ function displayNumber(e) {
 
 function operation(e) {
   operationClicked = true;
+  operationValue = e.target.value;
+  console.log(operationValue);
 }
 
+// Number are being added but no math just concatonation FIX PRBLEM.
+
 function add(num1, num2) {
-  num1 = displayValue1.join("");
-  num2 = displayValue2.join("");
   let sum = num1 + num2;
   console.log(sum);
+  display.textContent = sum;
   return sum;
 }
 
 function subtract(num1, num2) {
   let difference = num1 - num2;
+  console.log(difference);
+  display.textContent = difference;
   return difference;
 }
 
 function multiply(num1, num2) {
   let product = num1 * num2;
+  console.log(product);
+  display.textContent = product;
   return product;
 }
 
 function divide(num1, num2) {
   let quotient = num1 / num2;
+  console.log(quotient);
+  display.textContent = quotient;
   return quotient;
 }
 
 function operate(number1, op, number2) {
-  number1 = displayValue1.join("");
-  number2 = displayValue2.join("");
+  number1 = Number(displayValue1.join(""));
+  number2 = Number(displayValue2.join(""));
+  op = operationValue;
   if (op === "+") {
     return add(number1, number2);
   } else if (op === "-") {
