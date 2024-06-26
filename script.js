57bbf01c3675c65b9691038f76279e56361ecba6
Moises Gomez <76762359+moisesg526@users.noreply.github.com>
2024-06-25 23:36:17 -0400

    Changed the arrays result into a string

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let display = document.querySelector(".display");
 let answer = document.querySelector("#equals");
 let clear = document.querySelector("#clear");
 
-let displayValue1 = [];
-let displayValue2 = [];
+let displayValue1 = "";
+let displayValue2 = "";
 let operationValue;
 let operationClicked = false;
 let clickCount = 0;
@@ -23,8 +23,8 @@ answer.addEventListener("click", operate);
 clear.addEventListener("click", clearScreen);
 
 function clearScreen() {
-  displayValue1 = [];
-  displayValue2 = [];
+  displayValue1 = "";
+  displayValue2 = "";
   display.textContent = "0";
   console.log(displayValue1);
   console.log(displayValue2);
@@ -33,19 +33,22 @@ function clearScreen() {
 function displayNumber(e) {
   let value = e.target.value;
   if (operationClicked === false) {
-    displayValue1.push(value);
-    display.textContent = displayValue1.join("");
+    displayValue1 += value;
+    display.textContent = displayValue1;
     console.log(`Value 1: ${displayValue1}`);
   } else if (operationClicked === true) {
-    displayValue2.push(value);
-    display.textContent = displayValue2.join("");
+    displayValue2 += value;
+    display.textContent = displayValue2;
     console.log(`Value 2: ${displayValue2}`);
   }
 }
 
 function operation(e) {
-  if (clickCount === 1) {
-    mulpitpleOperations();
+  if (clickCount > 1) {
+    operate();
+    displayValue1 = result;
+    displayValue2 = "";
+    display.textContent = displayValue1;
   }
   operationClicked = true;
   clickCount++;
@@ -54,75 +57,80 @@ function operation(e) {
 }
 
 /// create multiple operations function to run when the operation buttons are clicked multiple tinmes
-function mulpitpleOperations() {
-  result = operate();
-  console.log(result);
-  displayValue1 = result;
-  displayValue2 = [];
-}
+// function mulpitpleOperations() {
+//   result = operate();
+//   console.log(result);
+//   displayValue1 = result;
+//   displayValue2 = [];
+// }
 
 function add(num1, num2) {
-  let sum = num1 + num2;
-  console.log(sum);
-  display.textContent = sum;
+  // let sum = num1 + num2;
+  result = num1 + num2;
+  console.log(result);
+  display.textContent = result;
   operationClicked = false;
-  return sum;
+  return result;
 }
 
 function subtract(num1, num2) {
-  let difference = num1 - num2;
-  console.log(difference);
-  display.textContent = difference;
+  // let difference = num1 - num2;
+  result = num1 - num2;
+  console.log(result);
+  display.textContent = result;
   operationClicked = false;
-  displayValue1 = difference;
-  return difference;
+  displayValue1 = result;
+  return result;
 }
 
 function multiply(num1, num2) {
-  let product = num1 * num2;
-  console.log(product);
-  display.textContent = product;
+  // let product = num1 * num2;
+  result = num1 * num2;
+  console.log(result);
+  display.textContent = result;
   operationClicked = false;
-  displayValue1 = product;
-  return product;
+  displayValue1.push(result);
+  console.log("Hello " + displayValue1);
+  return result;
 }
 
 function divide(num1, num2) {
-  let quotient = num1 / num2;
+  // let quotient = num1 / num2;
+  result = num1 / num2;
   if (num2 === 0) {
     display.textContent = "Hell No!";
   } else {
-    display.textContent = quotient;
+    display.textContent = result;
     operationClicked = false;
-    displayValue1 = quotient;
+    displayValue1 = result;
   }
-  console.log(quotient);
-  return quotient;
+  console.log(result);
+  return result;
 }
 
 function operate(number1, op, number2) {
-  number1 = Number(displayValue1.join(""));
-  number2 = Number(displayValue2.join(""));
+  number1 = Number(displayValue1);
+  number2 = Number(displayValue2);
   op = operationValue;
   if (op === "+") {
     clickCount = 0;
-    displayValue1 = [];
-    displayValue2 = [];
+    displayValue1 = "";
+    displayValue2 = "";
     return add(number1, number2);
   } else if (op === "-") {
     clickCount = 0;
-    displayValue1 = [];
-    displayValue2 = [];
+    displayValue1 = "";
+    displayValue2 = "";
     return subtract(number1, number2);
   } else if (op === "X") {
     clickCount = 0;
-    displayValue1 = [];
-    displayValue2 = [];
+    displayValue1 = "";
+    displayValue2 = "";
     return multiply(number1, number2);
   } else if (op === "÷") {
     clickCount = 0;
-    displayValue1 = [];
-    displayValue2 = [];
+    displayValue1 = "";
+    displayValue2 = "";
     return divide(number1, number2);
   }
 }
